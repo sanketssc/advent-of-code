3b506ef6e9e680478b04a46f59f67a36f3872bc2
sanketssc <49080676+sanketssc@users.noreply.github.com>
2023-12-01 00:07:29 +0530

    init dir with sample project

---
+++ 2023/rust/sample/src/bin/part1.rs
@@ -0,0 +1,5 @@
+use sample::process_part1;
+fn main() {
+    let input = include_str!("./input1.txt");
+    println!("{:?}", process_part1(input))
+}
---
+++ 2023/rust/sample/src/bin/part2.rs
@@ -0,0 +1,5 @@
+use sample::process_part2;
+fn main() {
+    let input = include_str!("./input2.txt");
+    println!("{:?}", process_part2(input))
+}
